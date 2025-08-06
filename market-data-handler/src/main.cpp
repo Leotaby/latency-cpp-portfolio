968f4b8fd700a5ad5fcecc28a59e2ffb0b1c1aaf
Leo <tabbakhianhatef@gmail.com>
2025-08-06 07:43:52 -0700

    Create main.cpp

---
+++ market-data-handler/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    // TODO: implement feed handler benchmark
+    std::cout << "Market data handler benchmark placeholder."
+              << std::endl;
+    return 0;
+}
