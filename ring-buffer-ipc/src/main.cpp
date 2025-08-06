e57df226da6a9b5ac7233f62b3a868acfb000fc8
Leo <tabbakhianhatef@gmail.com>
2025-08-06 07:53:49 -0700

    Create main.cpp

---
+++ ring-buffer-ipc/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    // TODO: implement ring buffer benchmark
+    std::cout << "Ring buffer IPC benchmark placeholder."
+              << std::endl;
+    return 0;
+}
